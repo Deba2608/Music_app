9f2e021fb6a6b1d1834aac12d4d6b2afffb8a649
Deba2608 <debayan26082002@gmail.com>
2024-03-07 12:30:00 +0530

    issue

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`https://deba2608.github.io/${folder}/`)
+    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
     let response = await a.text();
     // console.log(response)
     let div = document.createElement("div");
